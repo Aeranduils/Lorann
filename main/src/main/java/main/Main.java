4657e682a991676f5e1b42587a49e52b98eca0f5
flori <flori@DESKTOP-F58S82D>
2018-05-30 13:46:46 +0200

    Initial commit

---
+++ main/src/main/java/main/Main.java
@@ -0,0 +1,29 @@
+package main;
+
+import java.sql.SQLException;
+
+import controller.ControllerFacade;
+import model.ModelFacade;
+import view.ViewFacade;
+
+public abstract class Main {
+
+	/**
+	 * 
+	 * The main method.
+	 *
+	 * @param args
+	 *            the arguments
+	 */
+
+	public static void main(final String[] args) {
+		final ControllerFacade controller = new ControllerFacade(new ViewFacade(), new ModelFacade());
+
+		try {
+			controller.start();
+		} catch (final SQLException exception) {
+			exception.printStackTrace();
+		}
+	}
+
+}
